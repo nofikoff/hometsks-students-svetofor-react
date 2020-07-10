36f30b681fff47f67d60f60145fabaaf5ecc5984
nofikoffgit <ruslan.novikov@gmail.com>
2020-07-10 18:46:11 +0300

    make it better

---
+++ precache-manifest.7484b7baab681540a93d12e4fe5a6adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ece0a0dfac5a62d6f39180171da05e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bdfe8ea8431d41fea56",
+    "url": "/static/css/main.a51d906e.chunk.css"
+  },
+  {
+    "revision": "cca02e5394cb1b274571",
+    "url": "/static/js/2.b7ddc5f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b7ddc5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdfe8ea8431d41fea56",
+    "url": "/static/js/main.398e0782.chunk.js"
+  },
+  {
+    "revision": "1289dc4d65c7a3477879",
+    "url": "/static/js/runtime-main.920e9d75.js"
+  }
+]);
